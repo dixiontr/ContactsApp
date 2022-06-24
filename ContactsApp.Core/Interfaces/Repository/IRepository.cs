87ef8bce31802c4d09c81d9ef2dac1c8494ba7ba
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-25 00:15:21 +0300

    Created IRepository.cs file

---
+++ ContactsApp.Core/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using ContactsApp.Core.Interfaces.Entity;
+
+namespace ContactsApp.Core.Interfaces.Repository
+{
+    public interface IRepository<T>  where T : IEntity
+    {
+        Task CreateAsync(T entity);
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T,bool>> filter);
+        Task<T> GetAsync(Guid id);
+        Task<T> GetAsync(Expression<Func<T,bool>> filter);
+        Task RemoveAsync(Guid id);
+        Task UpdateAsync(T entity);
+    }
+}
