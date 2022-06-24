dd2259958bfde91538f472e29152bba5239b9084
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-24 16:39:24 +0300

    ExceptionHandlingMiddleware is created. UnhandledException is created

---
+++ ContactsApp.Core/Customs/Exceptions/UnhandledException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace ContactsApp.Core.Customs.Exceptions
+{
+    public class UnhandledException : aBaseException
+    {
+        public UnhandledException()
+        {
+            StatusCode = HttpStatusCode.InternalServerError;
+            Message = "Beklenmedik bir hata oluştu";
+            Success = false;
+        }
+    }
+}
---
+++ ContactsApp.Core/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Text.Json;
+using ContactsApp.Core.Customs.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace ContactsApp.Core.Middlewares
+{
+
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                response.StatusCode = (int)HttpStatusCode.OK;
+                var result = JsonSerializer.Serialize(new UnhandledException().HandleException());
+                await response.WriteAsync(result);
+
+            }
+        }
+    }
+
+}
