f2997b536567d4d7423e78010a92cc6f4a48db87
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-24 23:53:35 +0300

    Created IEntity.cs interface

---
+++ ContactsApp.Core/Interfaces/Entity/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactsApp.Core.Interfaces.Entity
+{
+
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+        DateTimeOffset CreatedOn { get; set; }
+        DateTimeOffset UpdatedOn { get; set; }
+    }
+
+}
