b8ce54101bd8c6f7075fc2e21e60b2abd7dd35c8
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-24 16:38:40 +0300

    aBaseException.cs is created. This class will be used to return standard exception responses

---
+++ ContactsApp.Core/Customs/Exceptions/Base/aBaseException.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using ContactsApp.Core.Wrappers;
+
+namespace ContactsApp.Core.Customs.Exceptions
+{
+    public abstract class aBaseException
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message {get;set;}
+        public bool Success {get;set;}
+
+
+        public BaseResponse HandleException()
+        {
+            return new BaseResponse()
+            {
+                Status = this.StatusCode,
+                Message = this.Message,
+                Success = this.Success
+            };
+        }
+    }
+
+}
