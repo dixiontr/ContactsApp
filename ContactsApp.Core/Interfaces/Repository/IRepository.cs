c1a8659ee9a51e9fe782bbd2155c9c0ffc2d29b9
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-25 15:42:25 +0300

    EfCoreGenericRepository.cs created in Core Project

--- ContactsApp.Core/Interfaces/Repository/IRepository.cs
+++ ContactsApp.Core/Interfaces/Repository/IRepository.cs
@@ -5,12 +5,12 @@ namespace ContactsApp.Core.Interfaces.Repository
 {
     public interface IRepository<T>  where T : IEntity
     {
-        Task CreateAsync(T entity);
-        Task<IReadOnlyCollection<T>> GetAllAsync();
-        Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T,bool>> filter);
+        Task<T> CreateAsync(T entity);
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync(Expression<Func<T,bool>> filter);
         Task<T> GetAsync(Guid id);
         Task<T> GetAsync(Expression<Func<T,bool>> filter);
-        Task RemoveAsync(Guid id);
-        Task UpdateAsync(T entity);
+        T Remove(T entity);
+        T Update(T entity);
     }
 }
---
+++ ContactsApp.Core/Repository/EfCoreGenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using ContactsApp.Core.Interfaces.Entity;
+using ContactsApp.Core.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ContactsApp.Core.Repository
+{
+    public class EfCoreGenericRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
+    {
+        private DbContext _context;
+        public EfCoreGenericRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            var result = await _context.Set<TEntity>().AddAsync(entity);
+            return result.Entity;
+        }
+
+        public Task<List<TEntity>> GetAllAsync()
+        {
+            return _context.Set<TEntity>().ToListAsync();
+        }
+
+        public Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().Where(filter).ToListAsync();
+        }
+
+        public Task<TEntity> GetAsync(Guid id)
+        {
+            return _context.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().Where(filter).FirstOrDefaultAsync();
+        }
+
+        public TEntity Remove(TEntity entity)
+        {
+            return _context.Set<TEntity>().Remove(entity).Entity;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            return _context.Set<TEntity>().Update(entity).Entity;
+        }
+    }
+
+}
