29d875517a38fa5d7a69e717402c5221325c2745
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-25 19:09:23 +0300

    Created Repositories

---
+++ ContactsApp.ContactService/UnitOfWork/Repositories/ContactInformationRepository.cs
@@ -0,0 +1,16 @@
+﻿using ContactsApp.ContactService.Entities;
+using ContactsApp.Core.Interfaces.Repository;
+using ContactsApp.Core.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApp.ContactService.UnitOfWork.Repositories
+{
+
+    public class ContactInformationRepository : EfCoreGenericRepository<ContactInformation>, IRepository<ContactInformation>
+    {
+        public ContactInformationRepository(DbContext context) : base(context)
+        {
+        }
+    }
+
+}
---
+++ ContactsApp.ContactService/UnitOfWork/Repositories/PersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using ContactsApp.ContactService.Entities;
+using ContactsApp.Core.Interfaces.Repository;
+using ContactsApp.Core.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApp.ContactService.UnitOfWork.Repositories
+{
+
+    public class PersonRepository : EfCoreGenericRepository<Person>, IRepository<Person>
+    {
+        public PersonRepository(DbContext context) : base(context)
+        {
+        }
+    }
+
+}
