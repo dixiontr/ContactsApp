838dccc834a7abb91eff27c565811bd7611076fb
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-25 14:47:57 +0300

    ContactContext.cs created

---
+++ ContactsApp.ContactService/Context/ContactContext.cs
@@ -0,0 +1,25 @@
+﻿using ContactsApp.ContactService.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace ContactsApp.ContactService.Context
+{
+
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options){ }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<ContactInformation> ContactInformations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<ContactInformation>()
+                .Property(d => d.InformationType)
+                .HasConversion(new EnumToStringConverter<InformationType>());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
