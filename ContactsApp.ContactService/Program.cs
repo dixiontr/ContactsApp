102f17ac9b8bc857788682af3725c50d882266b4
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-25 15:02:05 +0300

    Connected to PostgreSQL database

--- ContactsApp.ContactService/Program.cs
+++ ContactsApp.ContactService/Program.cs
@@ -1,3 +1,5 @@
+using ContactsApp.ContactService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddPostgreSQL(builder.Configuration.GetConnectionString("PostgreSQL"));
 
 var app = builder.Build();
 
---
+++ ContactsApp.ContactService/Services/ServiceInjector.cs
@@ -0,0 +1,19 @@
+﻿using ContactsApp.ContactService.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApp.ContactService.Services
+{
+
+    public static class ServiceInjector
+    {
+        public static IServiceCollection AddPostgreSQL(this IServiceCollection serviceCollection, string connectionString)
+        {
+            serviceCollection.AddEntityFrameworkNpgsql().AddDbContext<ContactContext>(opt =>
+            opt.UseNpgsql(connectionString));
+
+            return serviceCollection;
+        }
+        
+    }
+
+}
