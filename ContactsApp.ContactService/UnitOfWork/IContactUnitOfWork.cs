9ffd0681dabea2975a0179c8ec09821a8f7c15dc
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-25 19:09:44 +0300

    ContactService UnitOfWork.cs file created

---
+++ ContactsApp.ContactService/UnitOfWork/IContactUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using ContactsApp.ContactService.Entities;
+using ContactsApp.Core.Interfaces.Repository;
+using ContactsApp.Core.Interfaces.UnitOfWork;
+
+namespace ContactsApp.ContactService.UnitOfWork
+{
+
+    public interface IContactUnitOfWork : IUnitOfWork
+    {
+        IRepository<Person> PersonRepository { get; }
+        IRepository<ContactInformation> ContactInformationRepository { get; }
+    }
+
+}
---
+++ ContactsApp.ContactService/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using ContactsApp.ContactService.Entities;
+using ContactsApp.ContactService.UnitOfWork.Repositories;
+using ContactsApp.Core.Interfaces.Repository;
+using ContactsApp.Core.Interfaces.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ContactsApp.ContactService.UnitOfWork
+{
+
+    public class UnitOfWork : IContactUnitOfWork
+    {
+        private DbContext _context;
+        private ContactInformationRepository _contactInformationRepository;
+        private PersonRepository _personRepository;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+        public Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return _context.Database.BeginTransactionAsync();
+        }
+
+        public Task RollbackAsync()
+        {
+            return _context.Database.RollbackTransactionAsync();
+        }
+
+        public Task CommitAsync()
+        {
+            return _context.Database.CommitTransactionAsync();
+        }
+
+        public IRepository<Person> PersonRepository => _personRepository ?? new PersonRepository(_context);
+        public IRepository<ContactInformation> ContactInformationRepository =>
+            _contactInformationRepository ?? new ContactInformationRepository(_context);
+    }
+
+}
