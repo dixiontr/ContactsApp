d80905212014772ae1443069e0db612239227b0c
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-26 14:31:56 +0300

    ReportDTOs.cs created

---
+++ ContactsApp.ReportService/DTOs/ReportDTOs.cs
@@ -0,0 +1,30 @@
+﻿using ContactsApp.Core.Interfaces.DTO;
+using ContactsApp.ReportService.Entities;
+
+namespace ContactsApp.ReportService.DTOs
+{
+
+    public record ReportDTO : IDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public ReportStatus Status { get; set; }
+    }
+
+    public record ReportDetailDTO : IDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public ReportStatus Status { get; set; }
+        public List<ReportDataDTO> ReportDatas { get; set; }
+    }
+
+    public record ReportDataDTO : IDto
+    {
+        public Guid Id { get; set; }
+        public string Location { get; set; }
+        public int PersonCount { get; set; }
+        public int PhoneNumberCount { get; set; }
+    }
+
+}
