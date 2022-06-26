1fd634f2a0e73516b5e5e10403c6f06308f725f3
dixiontr <71636792+dixiontr@users.noreply.github.com>
2022-06-26 14:40:56 +0300

    ReportMapper.cs added

---
+++ ContactsApp.ReportService/Extensions/ReportMapper.cs
@@ -0,0 +1,42 @@
+﻿using ContactsApp.ReportService.DTOs;
+using ContactsApp.ReportService.Entities;
+
+namespace ContactsApp.ReportService.Extensions
+{
+
+    public static class ReportMapper
+    {
+        public static ReportDTO AsReportDTO(this Report report)
+        {
+            return new ReportDTO()
+            {
+                Id = report.Id,
+                CreatedOn = report.CreatedOn,
+                Status = report.Status
+            };
+        }
+
+        public static ReportDetailDTO AsReportDetailDTO(this Report report)
+        {
+            return new ReportDetailDTO()
+            {
+                Id = report.Id,
+                CreatedOn = report.CreatedOn,
+                Status = report.Status,
+                ReportDatas = report.ReportDatas.Select(x => x.AsReportDataDTO()).ToList()
+            };
+        }
+
+        public static ReportDataDTO AsReportDataDTO(this ReportData reportData)
+        {
+            return new ReportDataDTO()
+            {
+                Id = reportData.Id,
+                Location = reportData.Location,
+                PersonCount = reportData.PersonCount,
+                PhoneNumberCount = reportData.PhoneNumberCount
+            };
+        }
+    }
+
+}
